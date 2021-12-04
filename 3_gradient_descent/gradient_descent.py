6a4d48bdde9903232e28173b74ed756dbf5a50d7
alaminkawsar <alaminkawsar8643@gmail.com>
2021-12-05 03:53:36 +0600

    gradient_descent

--- 3_gradient_descent/gradient_descent.py
+++ 3_gradient_descent/gradient_descent.py
@@ -1,19 +1,21 @@
 import numpy as np
 
 def gradient_descent(x,y):
-    iterations = 10000
-    m_curr=b_curr=0
-    learning_rate = 0.00001
+    iterations=10000
+    learning_rate = 0.08
     n = len(x)
+    m_curr=b_curr=0
 
-    for i in range(0,iterations):
-        y_predicted = m_curr*x - b_curr
-        bd = -(2/n) * sum(y - y_predicted)
-        md = -(2/n) * sum(x * (y - y_predicted))
-        b_curr = b_curr - learning_rate * bd
-        m_curr = m_curr - learning_rate * md
-        print()
+    for i in range(iterations):
+        y_predicted = m_curr*x + b_curr
+        cost = (1/n)*sum([val**2 for val in (y-y_predicted)])
+        md = -(2/n)*sum(x*(y-y_predicted))
+        bd = -(2/n)*sum(y-y_predicted)
+        m_curr = m_curr - learning_rate*md
+        b_curr = b_curr - learning_rate*bd
+        print("m {}, b {}, cost {} iteration {}".format(m_curr,b_curr,cost,i))
 
+x = np.array([1,2,3,4,5])
+y = np.array([5,7,9,11,13])
 
-x = [1,2,3,4,5]
-y = [5,7,9,11,13]
+gradient_descent(x,y)
